abdab167a11dc58d912337db48c175050ffd0372
Ihor Cherednichenko <34284347+Igor4er@users.noreply.github.com>
2023-08-02 22:50:08 +0300

    Update main.py

--- back/back/main.py
+++ back/back/main.py
@@ -1,6 +1,7 @@
+import sys
+sys.path.insert(0,'/path/to/mod_directory')
 from back.back.app import app
 import uvicorn
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
