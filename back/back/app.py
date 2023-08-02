f596ca3e24e15946d45f38cf47210aa7512fbfd9
writer-mng <volodymyrpetriv2207@gmail.com>
2023-08-02 22:21:20 +0300

    Added backend for registration and authentication

--- back/back/app.py
+++ back/back/app.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+from back.back.db.db import db
+from back.back.db import models
 from back.back.routers.auth import router as auth_router
+
+db.connect()
+db.create_tables([models.User])
+db.close()
 
 app = FastAPI()
 
--- back/back/routers/auth.py
+++ back/back/routers/auth.py
@@ -1,21 +1,46 @@
-from fastapi import APIRouter
 import jwt
 import datetime
+from fastapi import APIRouter, Depends
+from typing import Annotated
+import argon2
+from fastapi import Request, HTTPException
+from back.back.dto.user import User
+from back.back.db import models
+from back.back.auth.authentication import JWTBearer
 from back.back.settings import settings
 
 router = APIRouter(prefix="/auth", tags=['authentification'])
 
 
-@router.get('/signup')
-def signup():
+@router.post('/signin')
+def signin(login: str, password: str):
+    try:
+        user = models.User.get(login=login)
+    except Exception as e:
+        print(e)
+        raise HTTPException(404, detail="User not found")
+    if argon2.verify_password(bytes(user.hashed_password, 'utf-8'), bytes(password, 'utf-8')):
+        token = {"login": user.login, "exp": datetime.datetime.now() + datetime.timedelta(hours=2)}
+        secret = settings.SECRET
+        return {"key": jwt.encode(token, secret, algorithm="HS256")}
+    else:
+        raise HTTPException(403, detail="Wrong password")
+
+
+# async def signin(user: Annotated[User, Depends(JWTBearer())]):
+#     return user
+
+
+@router.get('/dec')
+def dec(token: str):
     secret = settings.SECRET
-    token = {"exp": datetime.datetime.now() + datetime.timedelta(hours=1)}
-    encoded_token = jwt.encode(token, secret, algorithm="HS256")
-    return encoded_token
+    decoded_token = jwt.decode(token, secret, algorithms=["HS256"])
+    print(decoded_token)
+    return decoded_token
 
 
-@router.get('/signin')
-def signup(token: str):
-    return "signin"
-    # decoded_token = jwt.decode(token, "secret", algorithms=["HS256"])
-    # return decoded_token
+@router.post('/signup')
+def signup(login: str, password: str):
+    hashed_password = argon2.hash_password(bytes(password, 'utf-8'))
+    user, created = models.User.get_or_create(login=login, hashed_password=hashed_password)
+    return created
