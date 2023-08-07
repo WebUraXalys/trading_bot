6b2375ebe42f5446961c12f16421b0d8431d0ce0
writer-mng <volodymyrpetriv2207@gmail.com>
2023-08-07 13:13:04 +0300

    Updated POST request for user/settings

--- back/back/dto/user.py
+++ back/back/dto/user.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class User(BaseModel):
     login: str
     exp: int
+
+
+class UserSettings(BaseModel):
+    api_key: str | None = None
+    secret_key: str | None = None
--- back/back/routers/user.py
+++ back/back/routers/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated
 from fastapi import HTTPException
-from back.dto.user import User
+from back.dto.user import User, UserSettings
 from back.db import models
 from back.auth.authentication import JWTBearer
 
@@ -9,13 +9,13 @@ router = APIRouter(prefix="/user", tags=['user operations'])
 
 
 @router.post('/settings')
-async def set_settings(user: Annotated[User, Depends(JWTBearer())], api_key: str | None = None, secret_key: str | None = None, ):
+async def set_settings(user: Annotated[User, Depends(JWTBearer())], params: UserSettings):
     user = models.User.get(login=user.login)
     user_settings, created = models.UserSettings.get_or_create(user=user.id)
-    user_settings.api_key = api_key
-    user_settings.secret_key = secret_key
+    user_settings.api_key = params.api_key
+    user_settings.secret_key = params.secret_key
     user_settings.save()
-    return {"created": created}
+    return {"status": 200}
 
 
 @router.get('/settings')
