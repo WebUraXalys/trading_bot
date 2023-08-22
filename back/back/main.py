01fb94acffaa720d964208268347926544e2b3a8
writer-mng <volodymyrpetriv2207@gmail.com>
2023-08-22 19:55:31 +0300

    Fixed main.py

--- back/back/main.py
+++ back/back/main.py
@@ -1,6 +1,6 @@
-from back.back.app import app
+from back.app import app
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("back.main:app", host="0.0.0.0", port=8000, reload=True)
     
