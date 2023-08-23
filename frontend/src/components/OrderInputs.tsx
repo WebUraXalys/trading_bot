850bb4c25c9b7004b8cdcd5234ed2eca239e80ef
xalys_ura <xalysjyra1992@gmail.com>
2023-08-23 12:14:37 +0300

    Scripts for menu LIMIT OR MARKET

--- frontend/src/components/OrderInputs.tsx
+++ frontend/src/components/OrderInputs.tsx
@@ -1,33 +1,38 @@
-import { component$, useContext} from "@builder.io/qwik";
+import { component$, useContext, useComputed$, type Signal, useSignal} from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
-
+// interface ItemProps {
+//   limit?: string;
+//   market?: string;
+// }
+// export const Item = component$<ItemProps>((props) => {
+//   return (
+//       <div class="tabs">
+//          <a class={[ props.limit, 'tab']} onClick$={() => {}}>Limit</a> {/*  */}
+//          <a class={[ props.market, 'tab']} onClick$={() => {props.limit = "tab"; props.limit = "tab";  console.log(props.limit); console.log(props.market)}}>Market</a> 
+//       </div>
+//   );
+// });
 
 
 export default component$(() => {
+   // const activeBtn = useComputed$(() => {
+   // })
+   const limitBtn = useSignal('tab');
+   const marketBtn = useSignal('tab-active');
+
 
     return (
         <>
+      {/* <Item market="" limit="" /> */}
          <div class="tabs">
-            <a class="tab">Limit</a> 
-            <a class="tab tab-active">Market</a> 
+            <a class={[ limitBtn.value, 'tab']} onClick$={() => {marketBtn.value = 'tab'; limitBtn.value='tab-active'}}>Limit</a> 
+            <a class={[ marketBtn.value, 'tab']} onClick$={() => {limitBtn.value = 'tab'; marketBtn.value='tab-active'}}>Market</a> 
         </div>
-         <div class="input_for_limit">
+        {marketBtn.value == "tab-active" ? <div class="input_for_market">
             <div class="form-control">
-               <label class="label">
+               {/* <label class="label">
                   <span class="label-text">Enter amount</span>
-               </label>
-               <label class="input-group">
-                  <span>Size</span>
-                  <input type="text" placeholder="10" class="input input-bordered" />
-                  <span>USDT</span>
-               </label>
-            </div>
-         </div>
-         <div class="input_for_market">
-            <div class="form-control">
-               <label class="label">
-                  <span class="label-text">Enter amount</span>
-               </label>
+               </label> */}
                <label class="input-group">
                   <span>Price</span>
                   <input type="text" placeholder="10" class="input input-bordered" />
@@ -35,16 +40,36 @@ export default component$(() => {
                </label>
             </div>
             <div class="form-control">
-               <label class="label">
+               {/* <label class="label">
                   <span class="label-text">Enter amount</span>
-               </label>
+               </label> */}
                <label class="input-group">
                   <span>Size</span>
                   <input type="text" placeholder="10" class="input input-bordered" />
                   <span>USDT</span>
                </label>
             </div>
-         </div>
-        </>
+         </div> : <div class="input_for_limit">
+            <div class="form-control">
+               {/* <label class="label">
+                  <span class="label-text">Enter amount</span>
+               </label> */}
+               <label class="input-group">
+                  <span>Size</span>
+                  <input type="text" placeholder="10" class="input input-bordered" />
+                  <span>USDT</span>
+               </label>
+            </div>
+         </div>}
+         
+         </>
     )
 });
+
+
+
+
+ 
+
+ 
+
