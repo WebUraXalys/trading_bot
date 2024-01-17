60313a5650385cc6b0ffe16886ba87afdd683d1b
xalys_ura <xalysjyra1992@gmail.com>
2024-01-17 18:23:52 +0200

    Json

--- bot/main.py
+++ bot/main.py
@@ -10,7 +10,7 @@ print(api_key, api_secret)
 
 def main():
 
-    symbol = 'BNBBTC'
+    symbol = 'BTCUSDT'
 
     twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret, testnet=True)
     # start is required to initialise its internal loop
@@ -23,12 +23,12 @@ def main():
     twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
 
     # multiple sockets can be started
-    twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
+    # twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
 
     # or a multiplex socket can be started like this
     # see Binance docs for stream names
-    streams = ['bnbbtc@miniTicker', 'bnbbtc@bookTicker']
-    twm.start_multiplex_socket(callback=handle_socket_message, streams=streams)
+    # streams = ['bnbbtc@miniTicker', 'bnbbtc@bookTicker']
+    # twm.start_multiplex_socket(callback=handle_socket_message, streams=streams)
 
     twm.join()
 
