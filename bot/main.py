fad950344e5e4956325276a6ed07afdb5194e7db
Igor4er <sherednihenko.ihor@gmail.com>
2024-01-18 15:24:21 +0200

    Added klines filtering and logging

--- bot/main.py
+++ bot/main.py
@@ -8,6 +8,35 @@ api_key = os.environ.get("API_KEY")
 api_secret = os.environ.get("API_KEY_SECRET")
 print(api_key, api_secret)
 
+KLINES = []
+KLINEINFO = {
+
+}
+
+def handle_socket_message(msg):
+    gettime = int(time.time())
+    k = msg["k"]
+    if k["x"]:
+        if len(KLINEINFO) <= 0:
+            KLINEINFO.update({
+                "symbol": msg["s"],
+                "interval": k["i"]
+            })
+        kline= {
+            "start_time": k["t"],
+            "end_time": k["T"],
+            "open_price": k["o"],
+            "close_price": k["c"],
+            "high_price": k["h"],
+            "low_price": k["l"],
+            "kline_finished": k["x"]
+        }
+        KLINES.append(kline)
+        print(KLINEINFO, KLINES)
+    else:
+        print("TTK:", int(str(k["T"])[:10])-gettime)
+
+
 def main():
 
     symbol = 'BTCUSDT'
@@ -15,10 +44,6 @@ def main():
     twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret, testnet=True)
     # start is required to initialise its internal loop
     twm.start()
-
-    def handle_socket_message(msg):
-        print(f"message type: {msg['e']}")
-        print(msg)
 
     twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
 
