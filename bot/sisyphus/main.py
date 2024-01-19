be2b1cd8c2cb4811289fd6278a5bcd1111e5ba5a
Igor4er <sherednihenko.ihor@gmail.com>
2024-01-19 08:09:40 +0200

    Just refactor in 8am, nothing special

--- bot/sisyphus/main.py
+++ bot/sisyphus/main.py
@@ -2,7 +2,7 @@ import time
 from typing import List
 from binance import ThreadedWebsocketManager
 from settings import SETTINGS
-from models import Kline, KlineInfo
+from models import Kline
 
 api_key = SETTINGS.BINANCE_API_KEY
 api_secret = SETTINGS.BINANCE_API_SECRET
@@ -24,22 +24,12 @@ def handle_socket_message(msg):
 
 
 def main():
-
     symbol = 'BTCUSDT'
 
     twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret, testnet=True)
-    # start is required to initialise its internal loop
     twm.start()
 
     twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
-
-    # multiple sockets can be started
-    # twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
-
-    # or a multiplex socket can be started like this
-    # see Binance docs for stream names
-    # streams = ['bnbbtc@miniTicker', 'bnbbtc@bookTicker']
-    # twm.start_multiplex_socket(callback=handle_socket_message, streams=streams)
 
     twm.join()
 
